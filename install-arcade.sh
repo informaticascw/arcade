07666c2f415ac4039fce0535e11c237ca8eb96bf
Fabsterschu <84074851+Fabsterschu@users.noreply.github.com>
2025-01-12 15:11:15 +0100

    Add files via upload

---
+++ install-arcade.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Checking for internet connection..."
+
+#Testing internet connectivity until connected at 1 minute intervals
+ONLINE=1
+while [ $ONLINE -ne 0 ]
+do
+#Pinging a frequently used google DNS server
+	ping -c 1 8.8.8.8 > /dev/null 2>&1
+	ONLINE=$?
+	if [ $ONLINE -ne 0 ]
+	then
+		echo "Couldn't establish an internet connection."
+		sleep 60
+	fi
+done
+echo "Internet Connected!"
+#The rest of the code
