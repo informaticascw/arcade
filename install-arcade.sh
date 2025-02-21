e58424f444db3caaf3f6c848530d581a5db78371
Fabsterschu <84074851+Fabsterschu@users.noreply.github.com>
2025-02-21 16:49:06 +0100

    Add files via upload

--- install-arcade.sh
+++ install-arcade.sh
@@ -2,7 +2,7 @@
 
 echo "Checking for internet connection..."
 
-#Testing internet connectivity until connected at 1 minute intervals
+#Testing internet connectivity until connected at 10 second intervals
 ONLINE=1
 while [ $ONLINE -ne 0 ]
 do
@@ -12,8 +12,20 @@ do
 	if [ $ONLINE -ne 0 ]
 	then
 		echo "Couldn't establish an internet connection."
-		sleep 60
+		sleep 10
 	fi
 done
 echo "Internet Connected!"
-#The rest of the code
+
+echo "Downloading files..."
+sudo apt install wget unzip
+
+wget https://github.com/leerlingenscw/2425-arcade/archive/refs/heads/main.zip
+rm -rf ~/Arcade/2425-arcade-main
+unzip -o ~/main.zip  -d ~/Arcade/
+rm -rf ~/main.zip
+bash ~/Arcade/2425-arcade-main/watchdog.sh
+#python ~/Arcade/2425-arcade-main/src/games/flappybird/main.py
+
+
+read -p "Press [Enter] to close..."
