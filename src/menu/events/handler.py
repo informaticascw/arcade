d017df088c7bc2a874a7d4cb7d089a2719fae3f5
Enrico <93526844+Spookie6@users.noreply.github.com>
2025-03-05 17:21:53 +0100

    Update handler.py

--- src/menu/events/handler.py
+++ src/menu/events/handler.py
@@ -4,6 +4,12 @@ import sys
 from util.data import data
 from util.constants import Constants
 from util.data import data
+from util.games import start_game
+
+game_positions = []
+for i in range(2):
+    for j in range(3):
+        game_positions.append(list(reversed([i, j])))
 
 class EventHandler():
     def __init__(self, menu):
@@ -37,5 +43,12 @@ class EventHandler():
             case pg.K_d:
                 print("D")
                 self.menu.selection[0] += 1
-        
+            case pg.K_q:
+                sel = self.menu.selection
+                slide = self.menu.slides[self.menu.slideIndex]
+
+                index = game_positions.index(sel)
+                print(index, slide.batch[index])
+                if slide.batch[index]:
+                    start_game(slide.batch[index].entrypoint)
         
