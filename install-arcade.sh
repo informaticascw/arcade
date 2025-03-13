15ed2b9015166768765cc2b1b55da20986d094c1
Fabsterschu <84074851+Fabsterschu@users.noreply.github.com>
2025-03-13 17:30:21 +0100

    added starting without internet possibility

--- install-arcade.sh
+++ install-arcade.sh
@@ -4,6 +4,16 @@ echo "Checking for internet connection..."
 
 #Testing internet connectivity until connected at 10 second intervals
 ONLINE=1
+TRIES=0
+
+start_with_internet() {
+	sudo apt install wget unzip
+	wget https://github.com/leerlingenscw/2425-arcade/archive/refs/heads/main.zip
+	rm -rf ~/Arcade/2425-arcade-main
+	unzip -o ~/main.zip  -d ~/Arcade/
+	rm -rf ~/main.zip
+}
+
 while [ $ONLINE -ne 0 ]
 do
 #Pinging a frequently used google DNS server
@@ -11,21 +21,22 @@ do
 	ONLINE=$?
 	if [ $ONLINE -ne 0 ]
 	then
-		echo "Couldn't establish an internet connection."
-		sleep 10
+ 		if $TRIES >= 7 then
+   			echo "No internet was found within a minute after starting up"
+      			echo "Starting Arcade without internet..."
+	 		break 1
+	 	else
+			echo "Couldn't establish an internet connection."
+			sleep 10
 	fi
 done
-echo "Internet Connected!"
+if [ $ONLINE -ne 0 ]
+then
+	echo "Internet Connected!"
+	echo "Downloading and updating Arcade..."
+ 	start_with_internet
 
-echo "Downloading files..."
-sudo apt install wget unzip
-
-wget https://github.com/leerlingenscw/2425-arcade/archive/refs/heads/main.zip
-rm -rf ~/Arcade/2425-arcade-main
-unzip -o ~/main.zip  -d ~/Arcade/
-rm -rf ~/main.zip
 bash ~/Arcade/2425-arcade-main/watchdog.sh
-# python ~/Arcade/2425-arcade-main/src/games/flappybird/main.py
 
 
 read -p "Press [Enter] to close..."
